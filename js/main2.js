8b59863843b9284c9801830e3c9dc6027c0ff78e
Ambiorix Martinez <ambiorix565@gmail.com>
2024-04-04 16:18:33 -0400

    Arreglos de rutas

--- js/main2.js
+++ js/main2.js
@@ -1,12 +1,11 @@
-//Background index 
-const imagenbgLogin  = document.querySelector("#bodyLogin");
+const imagenbgRegistro  = document.querySelector("#bodyRegistro");
 
 const imagenesFondo = ["background1.jpg", "background2.jpg", "background3.jpg", "background4.jpg", "background5.jpg"];
 
 function cambiarImagen(fondos) {
     let random = Math.floor(Math.random() * fondos.length); 
-    imagenbgLogin.style.backgroundImage = `url("./assets/img/${imagenesFondo[random]}")`
-}      
+    imagenbgRegistro.style.backgroundImage = `url("../assets/img/${imagenesFondo[random]}")`
+}
 
 window.onload = function() {
     cambiarImagen(imagenesFondo);
